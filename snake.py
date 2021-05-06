90f6d9cc9d463775c7326e4390aaee7bce3516a6
angieramirez1800 <83669406+angieramirez1800@users.noreply.github.com>
2021-05-06 14:33:13 -0500

    Comentarios generales
    Comentarios de todo el código modificado

--- snake.py
+++ snake.py
@@ -1,37 +1,56 @@
-from turtle import *
-from random import randrange
-from freegames import square, vector
+# Código modificado
+# David Damián Galán
+# Angélica Sofía Ramírez Porras
 
-food = vector(0, 0)
-snake = [vector(10, 0)]
-aim = vector(0, -10)
+from turtle import * # Importa todas las funciones de turtle
+from random import randrange # Importa randrange de random
+# Importa funciones vector y square de freegames
+from freegames import square, vector 
+food = vector(0, 0) # posición inicial de comida 
+snake = [vector(10, 0)] # posición inicial de snake
+aim = vector(0, -10) # vector de movimiento
+
 
 def change(x, y):
-    "Change snake direction."
+    """
+    Cambia la dirección de la snake
+    x = coordenada x
+    y = coordenada y 
+    """ 
     aim.x = x
     aim.y = y
 
+    
 def inside(head):
-    "Return True if head inside boundaries."
+    """ 
+    Para que la coordenada head esté adentro del recuadro 
+    head = coordenada evaluada
+    """
     return -200 < head.x < 190 and -200 < head.y < 190
 
+
 def move():
-    "Move snake forward one segment."
-    head = snake[-1].copy()
-    head.move(aim)
+    """ 
+    Mover la snake y la comida 
+    """
+    head = snake[-1].copy() # Copia la coordenada de la cabeza 
+    head.move(aim) # Mueve la cabeza
 
     if not inside(head) or head in snake:
+        # En caso de que la snake choque con el muro o sí misma, pierde 
         square(head.x, head.y, 9, 'red')
         update()
         return
 
-    snake.append(head)
+    snake.append(head) # Agrega nueva cabeza a snake
 
     if head == food:
+        # En caso de que sí coma la snake, crece
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
     else:
+        # En caso de que no coma la snake, se queda igual 
         snake.pop(0)
         # Crea la variable que indica si la comida ya cambio de posición
         food_changed = False
@@ -55,24 +74,29 @@ def move():
                 # El movimiento no fue válido
                 food_changed = False # No se ha movido la comida
 
-    clear()
+    clear() # Borrar las figuras 
 
     for body in snake:
-        # Escoge la posición y el color de la serpiente y la comida
+        # Dibuja la posición de snake y su color
         square(body.x, body.y, 9, snake_color) 
-
+        
+    # Coloca la comida y su color 
     square(food.x, food.y, 9, food_color)
-    update()
-    ontimer(move, 100)
+    update() # Actualiza el dibujo 
+    ontimer(move, 100) # Mover la snake cada 100 mili segundos
 
 
 def coloring_food():
     """
-Cambia el color de la comida en cada corrida aleatoriamente 
-"""
     
-    randcfood=randrange(0,5)# Escoge un valor entre 0 y 4
-  # Escoge un color diferente dependiendo del número  
+    Cambia el color de la comida en cada corrida aleatoriamente
+
+    """
+    
+    randcfood=randrange(0,5) # Escoge un valor entre 0 y 4
+    
+    # Escoge un color diferente dependiendo del número 
+
     if(randcfood==0):
         return 'BLUE'
     
@@ -91,11 +115,15 @@ Cambia el color de la comida en cada corrida aleatoriamente
 
 def coloring_snake():
     """
-Cambia el color de la serpiente en cada corrida aleatoriamente 
-"""
+    
+    Cambia el color de la serpiente en cada corrida aleatoriamente
+    
+    """
     
     randcsnake=randrange(0,5)# Escoge un valor entre 0 y 4 
+    
     # Escoge un color diferente dependiendo del número 
+    
     if(randcsnake==0):
         return 'ORANGE'
     
@@ -111,19 +139,28 @@ Cambia el color de la serpiente en cada corrida aleatoriamente
     elif(randcfood==4):
          return 'MAGENTA'
 
-# Guarda el color aleatorio de la comida  
+        
+# Guarda el color aleatorio de la comida 
+
 food_color = coloring_food()
+
 # Guarda el color aleatorio de la serpiente
+
 snake_color = coloring_snake()
 
+setup(420, 420, 370, 0) # Dimensiones del recuadro
+hideturtle() # Esconde la flecha 
+tracer(False) # Esconde animación de square
+listen() # Captura los eventos del teclado
 
-setup(420, 420, 370, 0)
-hideturtle()
-tracer(False)
-listen()
+# Los controles del juego en el teclado 
+
 onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
+
+# Inicia el juego
 move()
+
 done()
